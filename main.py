aeb1765dc542d12e8dc1738a39e2eb982ff158bd
ConnorT161 <102322583+ConnorT161@users.noreply.github.com>
2022-04-14 19:26:43 +0000

    made it so you can choose between two flags and finished france

--- main.py
+++ main.py
@@ -3,40 +3,76 @@ import turtle
 
 t = turtle.Turtle()
 
-t.forward(100)
-t.right(180)
-for x in range (2):
-  t.forward(200)
-  t.right(90)
-  t.forward(100)
-  t.right(90)
+_input = input("France or Japan flag?\n")
+  
+_input = _input.lower()
 
-t.forward(100)
-t.right(90)
-t.pu()
-t.forward(20)
-t.right(90)
-t.pd()
-t.color("red", "red")
-t.begin_fill()
-for x in range (180):
-  t.forward(1)
-  t.left(2)
-t.end_fill()
-t.color("black","white")
-t.pu()
-t.right(90)
-t.forward(20)
-t.right(90)
-t.forward(100)
-t.left(180)
-t.pd()
-for x in range (2):
-  t.forward(200)
-  t.right(90)
+
+def japan():
+  
+  t.color("black","white")
+  t.forward(100)
+  t.right(180)
+  for x in range (2):
+    t.forward(200)
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+  
   t.forward(100)
   t.right(90)
-t.forward(66)
-t.color("blue","blue")
-t.right(90)
-t.forward(100)
+  t.pu()
+  t.forward(20)
+  t.right(90)
+  t.pd()
+  t.color("red", "red")
+  t.begin_fill()
+  for x in range (180):
+    t.forward(1)
+    t.left(2) 
+  t.end_fill()
+
+
+def france():
+  t.color("black","white")
+  for x in range (2):
+    t.forward(200)
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+    t.begin_fill()
+    
+  for x in range (2):
+    t.forward(66)
+    t.color("blue","blue")
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+  t.forward(66)
+  t.end_fill()
+  t.pu()
+  t.forward(67)
+  t.pd()
+  t.color("red","red")
+  t.begin_fill()
+  for x in range (2):
+    t.forward(66)
+    
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+  t.end_fill()
+
+while True:
+  if _input == ("japan"):
+    japan()
+  elif _input == ("france"):
+    france()
+  else:
+    print("try again")
+  _input = input("France or Japan flag?\n")
+  t.clear()
+  _input = _input.lower()
+
+
+  
